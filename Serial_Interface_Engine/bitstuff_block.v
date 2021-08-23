8c6263bc155f954274f6486d6974582db885e231
JeffreySamuel <85092975+JeffreySamuel@users.noreply.github.com>
2021-08-23 22:46:45 +0530

    Update bitstuff_block.v

--- Serial_Interface_Engine/bitstuff_block.v
+++ Serial_Interface_Engine/bitstuff_block.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12.06.2021 09:50:49
-// Design Name: 
-// Module Name: bitstuff_block
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 //Finalised
 module bitstuff_block(
